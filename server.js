78e7f16fec984790121ff78f91867c154a98e951
JStuve <stuve.josh@gmail.com>
2019-01-15 15:16:09 -0600

    Final config. Ready for server

--- server.js
+++ server.js
@@ -13,20 +13,24 @@ app.listen(port, () => {
 
 app.post('/', (req, res) => {
 
-  // const body = req.body;
+  const body = req.body;
   branch = body["ref"].split("/").pop();
   console.log(branch)
-  // if(branch == "dev"){
-  //   console.log("Run Dev Script");
-  //   shell.exec('./deploy-dev.sh');
-  // }
-  // else if (branch == "exp"){
-  //   console.log("Run Exp Script");
-  //   shell.exec('./deploy-exp.sh');
-  // }
-  // else {
-  //   console.log("Do nothing. BRANCH: " + str(branch))
-  // }
+  if(branch == "dev"){
+    console.log("Run Dev Script");
+    shell.exec('./deploy-dev.sh');
+  }
+  else if (branch == "exp"){
+    console.log("Run Exp Script");
+    shell.exec('./deploy-exp.sh');
+  }
+  else if (branch == "master"){
+    console.log("Run MASTER Script");
+    shell.exec('./deploy-master.sh');
+  }
+  else {
+    console.log("Do nothing. BRANCH: " + str(branch))
+  }
   res.send({
     'statusCode' : 200,
     'body' : 'Finished Request From: ' + branch
